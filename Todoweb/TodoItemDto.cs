21e2211b7d46696b80d451bc895f15b4fca6be14
KKondor <kkondor66@gmail.com>
2026-07-07 14:44:47 +0200

    feat: added a Data Transfer Object for

---
+++ Todoweb/TodoItemDto.cs
@@ -0,0 +1,19 @@
+﻿namespace Todoweb
+{
+    public class TodoItemDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public Todo.Priority TodoPriority { get; set; } = Todo.Priority.Low_Priority;
+        public string? Description { get; set; }
+        public bool IsComplete { get; set; } = false;
+        public DateTime CreateDate { get; } = DateTime.Now;
+        public DateTime? DueDate { get; set; }
+
+        public TodoItemDto() { }
+        public TodoItemDto(Todo todoItem) =>
+        (Id, Name, TodoPriority, Description, IsComplete, CreateDate, DueDate) = 
+            (todoItem.Id, todoItem.Name, todoItem.TodoPriority, todoItem.Description, todoItem.IsComplete, todoItem.CreateDate, todoItem.DueDate);
+
+    }
+}
