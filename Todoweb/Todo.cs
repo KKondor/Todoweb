7d94ae34a91c6363c069b88637684a9a22ec6241
KKondor <kkondor66@gmail.com>
2026-07-07 13:10:56 +0200

    Add project files.

---
+++ Todoweb/Todo.cs
@@ -0,0 +1,20 @@
+﻿namespace Todoweb
+{
+    public class Todo
+    {
+        public enum Priority
+        {
+            Low_Priority,
+            Normal_Priority,
+            Urgent_Priority
+        }
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public Priority TodoPriority { get; set; }
+        public string? Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreateDate { get; }
+        public DateTime DueDate { get; set; }
+
+    }
+}
---
+++ Todoweb/TodoDb.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Todoweb
+{
+    public class TodoDb : DbContext
+    {
+        public TodoDb
+    }
+}
